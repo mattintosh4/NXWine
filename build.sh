35ae6c95c433f0457d83b7cffa992b31e4b9c14d
mattintosh4 <mattintosh4@gmail.com>
2013-04-27 23:18:21 +0900

    no message

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git_dir=/usr/local/git/bin
 python_dir=/Library/Frameworks/Python.framework/Versions/2.7/bin
 
 test -x /usr/local/bin/ccache && ccache=$_ || exit
-test -x /usr/local/bin/clang && { clang=$_; clangxx="${clang} -x c++ -stdlib=libstdc++"; } || exit
+test -x /usr/local/bin/clang && { clang=$_; clangxx=${clang}++; } || exit
 test -x /usr/local/bin/make && make=$_ || exit
 test -x /usr/local/bin/uconv && uconv=$_ || exit
 test -x /usr/local/bin/pkg-config && {

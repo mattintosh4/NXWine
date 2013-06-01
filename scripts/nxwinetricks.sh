5f921b99fb4bad6ac5910266f3da222241dbfc8e
mattintosh4 <mattintosh4@gmail.com>
2013-06-02 03:56:25 +0900

    Update nxwinetricks.sh

--- scripts/nxwinetricks.sh
+++ scripts/nxwinetricks.sh
@@ -5,6 +5,7 @@
 # Created by mattintosh4 on @DATE@.
 # Copyright (C) 2013 mattintosh4, https://github.com/mattintosh4/NXWine
 #
+LANG=${LANG:=ja_JP.UTF-8}
 
 usage="Usage: $(cd "$(dirname "$0")" && pwd)/nxwinetricks [package]
 
@@ -44,28 +45,27 @@ function install_rpg2000 {
     # f1ea2dd0610d005282f3840c349754cdece9f3ad
     curl -o $1 'http://ftp.vector.co.jp/pack/winnt/util/runtime/2000rtp.zip'
     ${extract} -o'c:\tkool\2000' $1
-    ${wine} 'c:\tkool\2000\利用規約.txt'
     ${wine} 'c:\tkool\2000\RPG2000RTP.exe'
+    ${wine} 'c:\tkool\2000\使用規約.txt'
 }
 function install_rpg2003 {
     # 9a63d4e58d752d6ed5de79492a31ce43d0060564
     curl -o $1 'http://ftp.vector.co.jp/pack/winnt/util/runtime/2003rtp.zip'
     ${extract} -o'c:\tkool\2003' $1
-    ${wine} 'c:\tkool\2003\利用規約.txt'
     ${wine} 'c:\tkool\2003\RPG2003RTP.exe'
+    ${wine} 'c:\tkool\2003\使用規約.txt'
 }
 function install_rpgxp {
     # 2079f38b692569c1fc734320862badb170bbd29d
     curl -o $1 'http://ftp.vector.co.jp/pack/winnt/util/runtime/xp_rtp103.zip'
     ${extract} -o'c:\tkool\XP103' $1
-    ${wine} 'c:\tkool\XP103\利用規約.txt'
     ${wine} 'c:\tkool\XP103\Setup.exe'
+    ${wine} 'c:\tkool\XP103\利用規約.txt'
 }
 function install_rpgvx {
     # 351b4e528dc6ed4ed9988f0a636da6b1df48d6f2
     curl -o $1 'http://ftp.vector.co.jp/pack/winnt/util/runtime/vx_rtp202.zip'
     ${extract} -o'c:\tkool\VX202' $1
-    ${wine} 'c:\tkool\VX202\利用規約.txt'
     ${wine} 'c:\tkool\VX202\Setup.exe'
 }
 

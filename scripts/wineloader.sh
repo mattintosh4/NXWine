d3705573348c04db7fd6fb3f5ea493067acbb2d3
mattintosh4 <mattintosh4@gmail.com>
2013-06-02 16:16:16 +0900

    Update wineloader.sh

--- scripts/wineloader.sh
+++ scripts/wineloader.sh
@@ -8,8 +8,7 @@
 
 # note: some debug options is enabled because this script is incomplete yet.
 PS4="\[\e[33m\]DEBUG:\[\e[m\] "
-set -x
-case ${WINEDEBUG-X} in X) export WINEDEBUG=+loaddll ;; esac
+case ${WINEDEBUG-X} in X) set -x; export WINEDEBUG=+loaddll;; esac
 
 # ------------------------------------ begin preparing
 prefix=/Applications/NXWine.app/Contents/Resources

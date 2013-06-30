61515c1b5403b38379db044e98e73efdfa3b12e1
mattintosh4 <mattintosh4@gmail.com>
2013-06-30 16:42:59 +0900

    no message

--- scripts/nxwinetricks.sh
+++ scripts/nxwinetricks.sh
@@ -71,7 +71,7 @@ function ConvLess_ {
 # -------------------------------------
 function install_rpg2000 {
     # f1ea2dd0610d005282f3840c349754cdece9f3ad
-    set 2000rtp.zip
+    set -- 2000rtp.zip
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/winnt/util/runtime/2000rtp.zip'
     ${FlatExtract} $1
     ${wine} RPG2000RTP.exe
@@ -79,7 +79,7 @@ function install_rpg2000 {
 }
 function install_rpg2003 {
     # 9a63d4e58d752d6ed5de79492a31ce43d0060564
-    set 2003rtp.zip
+    set -- 2003rtp.zip
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/winnt/util/runtime/2003rtp.zip'
     ${FlatExtract} $1
     ${wine} RPG2003RTP.exe
@@ -87,7 +87,7 @@ function install_rpg2003 {
 }
 function install_rpgxp {
     # 2079f38b692569c1fc734320862badb170bbd29d
-    set xp_rtp103.zip
+    set -- xp_rtp103.zip
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/winnt/util/runtime/xp_rtp103.zip'
     ${FlatExtract} $1
     ${wine} Setup.exe
@@ -95,7 +95,7 @@ function install_rpgxp {
 }
 function install_rpgvx {
     # 351b4e528dc6ed4ed9988f0a636da6b1df48d6f2
-    set vx_rtp202.zip
+    set -- vx_rtp202.zip
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/winnt/util/runtime/vx_rtp202.zip'
     ${FlatExtract} $1
     ${wine} setup.exe
@@ -104,20 +104,20 @@ function install_rpgvx {
 
 # -------------------------------------
 function install_aooni {
-    set aooni.zip
+    set -- aooni.zip
     [ -f $1 ] || curl -O 'http://mygames888.info/zip/aooni.zip'
     unzip -o $1
     ${PathExtract} -o'c:\Program Files\aooni' $(basename $1 .zip).exe
     less "$(${winepath} 'c:\Program Files\aooni\README.txt')"
 }
 function install_ib {
-    set Ib_1.05.zip
+    set -- Ib_1.05.zip
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/win95/game/avg/horror/Ib_1.05.zip'
     ${PathExtract} -o'c:\Program Files' $1
     ConvLess_ "$(${winepath} 'c:\Program Files\Ib_1.05\Ib_説明書.txt')"
 }
 function install_yumenikki {
-    set yumenikki0.10.lzh yumesyuusei.lzh
+    set -- yumenikki0.10.lzh yumesyuusei.lzh
     [ -f $1 ] || curl -O 'http://ftp.vector.co.jp/pack/win95/game/avg/yumenikki0.10.lzh'
     [ -f $2 ] || curl -O 'http://www3.nns.ne.jp/pri/tk-mto/yumesyuusei.lzh'
     ${PathExtract} -o'c:\Program Files' $1
@@ -149,5 +149,5 @@ printf "checking 7z.exe... "; [ -f "${sevenzip}" ] && printf "${sevenzip}\n" || 
 
 PS4=
 set -x
-install -d /Users/$(whoami)/Library/Caches/com.github.mattintosh4.NXWine/$1 && cd $_
+mkdir -p /Users/$(whoami)/Library/Caches/com.github.mattintosh4.NXWine/$1 && cd $_
 install_$1

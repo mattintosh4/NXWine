c51f3aa9cd6db0326d6d33a352d18d58f42f8888
mattintosh4 <mattintosh4@gmail.com>
2013-08-05 23:54:35 +0900

    modified winetricks loader

--- scripts/winetricksloader.sh
+++ scripts/winetricksloader.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-prefix=/Applications/NXWine.app/Contents/Resources
-export PATH=${prefix}/bin:/usr/bin:/bin:/usr/sbin:/sbin
+if [ "${WINEPREFIX+set}" != set ]; then
+    export WINEPREFIX="$(osascript -e 'POSIX path of (path to library folder from user domain) & "NXWine/prefixies/default"')"
+fi
+export PATH=/Applications/NXWine.app/Contents/Resources/bin:/usr/bin:/bin:/usr/sbin:/sbin
 type wine || exit
-exec ${prefix}/libexec/winetricks "$@"
+exec /Applications/NXWine.app/Contents/Resources/libexec/winetricks "$@"

9ca4fec3e8176a33794d10c76b2e109d23d914c7
mattintosh4 <mattintosh4@gmail.com>
2013-08-10 07:17:34 +0900

    fixed to load zenity

--- scripts/winetricksloader.sh
+++ scripts/winetricksloader.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+
+export PATH=/Applications/NXWine.app/Contents/Resources/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+## WINEPREFIX
 if [ "${WINEPREFIX+set}" != set ]; then
     export WINEPREFIX="$(osascript -e 'POSIX path of (path to library folder from user domain) & "NXWine/prefixies/default"')"
 fi
-export PATH=/Applications/NXWine.app/Contents/Resources/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+## Wine
 type wine || exit
+
+## Zenity
+if [ -x "$ZENITY" ]; then
+    PATH+=:"$(dirname "$ZENITY")"
+elif [ -x /opt/local/bin/zenity ]; then
+    PATH+=:/opt/local/bin
+elif [ -x /usr/local/bin/zenity ]; then
+    PATH+=:/usr/local/bin
+fi
+
 exec /Applications/NXWine.app/Contents/Resources/libexec/winetricks "$@"

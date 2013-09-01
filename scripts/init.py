60572ff8a7fc04a36986758965ea91a42a15c513
mattintosh4 <mattintosh4@gmail.com>
2013-09-01 17:45:50 +0900

    updated init.py

--- scripts/init.py
+++ scripts/init.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+from subprocess import call, Popen, PIPE
 import fnmatch
 import os
 import re
 import shutil
-import subprocess
+import tempfile
+
+
+############
+# VARIABLE #
+############
 
 prefix      = "/usr/local/wine/"
-WINELOADER  = prefix + "bin/wine"
-w_system32  = os.path.expanduser('~/.wine/drive_c/windows/system32/')
-srcmedia    = '/Volumes/WindowsXPSP3/i386/'
+WINELOADER  = os.path.join(prefix, "bin/wine")
+SPSRC       = "/usr/local/src/NXWine/sources/nativedlls/WINDOWSXP-KB936929-SP3-X86-JPN.EXE"
+
+W_DRIVE_C   = Popen([WINELOADER, "winepath.exe", "-u", "c:"], stdout=PIPE).communicate()[0].rstrip()
+W_SYSTEM32  = os.path.join(W_DRIVE_C, "windows/system32/")
+W_TEMP      = os.path.join(W_DRIVE_C, "windows/temp", os.path.basename(tempfile.NamedTemporaryFile().name))
+
+print W_DRIVE_C
+print W_SYSTEM32
+print W_TEMP
+
+
+############
+# FUNCTION #
+############
 
 def wine (*args):
-    cmd = (WINELOADER,) + args
-    subprocess.call(cmd)
+    call((WINELOADER,) + args)
 
 def p7ze (src, dst):
-    subprocess.call(
-        ['/opt/local/bin/7z', 'e', '-y', '-ssc-', '-o' + dst, src],
-        stdout=open(os.devnull, 'w'))
-    print 'Extracted', src, '->', dst
+    call(['/opt/local/bin/7z', 'e', '-y', '-ssc-', '-o' + dst, src], stdout=open(os.devnull, 'w'))
+
+
 
 wine("rundll32.exe", "setupapi.dll,InstallHinfSection", "DefaultInstall", "128", "/usr/local/src/NXWine/inf/init.inf")
-
 
 as_dxnt = '''\
 amstream.dl_
@@ -129,19 +144,14 @@ wstcodec.sy_
 wstdecod.dl_
 '''.splitlines()
 
+array = []
 for f in as_dxnt + as_bdaxp:
-    
-    src = os.path.join(srcmedia, f)
-    dst = w_system32
-  
-    if f.endswith(('.sy_')):
-        dst += 'drivers'
-        
-    elif f.endswith(('.in_')):
-        dst += '../inf'
-    
-    p7ze(src, dst)
+    array.append(os.path.join("i386", f))
+call(["/opt/local/bin/7z", "e", "-y", "-ssc-", "-o" + W_TEMP, SPSRC] + array)
+del array
 
+for f in as_dxnt + as_bdaxp:
+    call(["/opt/local/bin/7z", "e", "-y", "-ssc-", "-o" + W_SYSTEM32, os.path.join(W_TEMP, f)])
 
 additions = """\
 d3dim.xpg
@@ -158,17 +168,15 @@ dsound.vxd
 
 for f in additions:
     
-    subprocess.call(
-        ["/opt/local/bin/7z", "e", "-y", "-o" + w_system32, "/usr/local/wine/share/wine/directx9/feb2010/dxnt.cab", f],
-        stdout=open(os.devnull, 'w'))
+    call(["/opt/local/bin/7z", "e", "-y", "-o" + W_SYSTEM32, "/usr/local/wine/share/wine/directx9/feb2010/dxnt.cab", f], stdout=open(os.devnull, 'w'))
     
     print 'Extracted file', f, "from dxnt.cab"
     
     if fnmatch.fnmatch(f, "dsound.vxd"):
         continue
     
-    src = os.path.join(w_system32, f)
-    dst = w_system32
+    src = os.path.join(W_SYSTEM32, f)
+    dst = W_SYSTEM32
     
     if fnmatch.fnmatch(f, "dxapi.xpg"):
         dst += "drivers/dxapi.sys"
@@ -216,7 +224,7 @@ reg = '''\
 "quartz"="native"
 '''
 
-subprocess.Popen([WINELOADER, 'regedit.exe', '-'], stdin=subprocess.PIPE).communicate(reg)
+Popen([WINELOADER, 'regedit.exe', '-'], stdin=PIPE).communicate(reg)
 
 # VisualBasic 6 sp 6
 # VB6.0-KB290887-X86.exe/vbrun60sp6.exe
@@ -528,161 +536,180 @@ files = \
 
 files = files.splitlines()
 
+array = []
 for f in files:
-    
-    src = srcmedia + f
-    dst = w_system32
+    array.append(os.path.join("i386", f))
+call(["/opt/local/bin/7z", "e", "-y", "-ssc-", "-o" + W_TEMP, SPSRC] + array)
+del array
+
+for f in files:
+    src = os.path.join(W_TEMP, os.path.basename(f))
+    dst = W_SYSTEM32
     
     if f.endswith((".sy_", ".sys")):
-        dst += "drivers"
+        dst = os.path.join(dst, "drivers")
         
     if f.endswith(("_")):
         p7ze(src, dst)
         
     else:
         shutil.copy2(src, dst)
-        print "Copied file", src, "->", os.path.join(dst, f)
         
     if f.endswith((".ex_", ".exe")):
-        os.chmod(dst + os.path.splitext(os.path.basename(f))[0] + ".exe", 0755)
+        os.chmod(os.path.join(dst, os.path.splitext(os.path.basename(f))[0] + ".exe"), 0755)
 
-subprocess.call(["/opt/local/bin/7z", "e", "-y", "-o" + w_system32, srcmedia + "root/cmpnents/netfx/i386/netfx.cab", "msvcp70.dll", "msvcr70.dll"])
+call(["/opt/local/bin/7z", "e", "-y", "-o" + W_TEMP, SPSRC, "i386/root/cmpnents/netfx/i386/netfx.cab"])
+call(["/opt/local/bin/7z", "e", "-y", "-o" + W_SYSTEM32, os.path.join(W_TEMP, "netfx.cab"), "msvcp70.dll", "msvcr70.dll"])
 
-RegisterDlls = """\
-actxprxy.dll
-advpack.dll
-asctrls.ocx
-atl.dll
-bdaplgin.ax
-browseui.dll
-ddrawex.dll
-devenum.dll
-dinput.dll
-dinput8.dll
-dispex.dll
-dmband.dll
-dmcompos.dll
-dmime.dll
-dmloader.dll
-dmscript.dll
-dmstyle.dll
-dmsynth.dll
-dmusic.dll
-dplayx.dll
-dpnet.dll
-dpnhpast.dll
-dpnhupnp.dll
-dpvacm.dll
-dpvoice.dll
-dpvvox.dll
-dsdmo.dll
-dsdmoprp.dll
-dsprop.dll
-dsquery.dll
-dssenh.dll
-dsuiext.dll
-dswave.dll
-dx7vb.dll
-dx8vb.dll
-dxdiagn.dll
-dxmasf.dll
-dxmrtp.dll
-dxtmsft.dll
-dxtrans.dll
-encapi.dll
-encdec.dll
-flash.ocx
-fontext.dll
-hhctrl.ocx
-ipsink.ax
-jscript.dll
-ksproxy.ax
-kswdmcap.ax
-l3codeca.acm
-mfc40u.dll
-mfc42.dll
-mfc42u.dll
-mp43dmod.dll
-mp4sdmod.dll
-mpg2data.ax
-mpg2splt.ax
-mpg4dmod.dll
-mpg4ds32.ax
-msadds32.ax
-msdvbnp.ax
-msdxm.ocx
-msscds32.ax
-msscript.ocx
-msvbvm60.dll
-mswebdvd.dll
-mswmdm.dll
-oledb32.dll
-oledb32r.dll
-oleprn.dll
-proctexe.ocx
-psisrndr.ax
-qasf.dll
-qcap.dll
-qcliprov.dll
-qdv.dll
-qdvd.dll
-qedit.dll
-quartz.dll
-query.dll
-qutil.dll
-scecli.dll
-shdocvw.dll
-shgina.dll
-shimgvw.dll
-shmedia.dll
-shsvcs.dll
-slbcsp.dll
-slbiop.dll
-stobject.dll
-sysmon.ocx
-tdc.ocx
-vbicodec.ax
-vbisurf.ax
-vbscript.dll
-vidcap.ax
-wmadmod.dll
-wmadmoe.dll
-wmdmlog.dll
-wmdmps.dll
-wmm2ae.dll
-wmm2ext.dll
-wmm2filt.dll
-wmm2fxa.dll
-wmm2fxb.dll
-wmp.ocx
-wmpcd.dll
-wmpcore.dll
-wmphoto.dll
-wmpui.dll
-wmsdmod.dll
-wmsdmoe.dll
-wmsdmoe2.dll
-wmspdmod.dll
-wmspdmoe.dll
-wmstream.dll
-wmv8ds32.ax
-wmvcore.dll
-wmvdmod.dll
-wmvdmoe2.dll
-wmvds32.ax
-wshcon.dll
-wshext.dll
-wshom.ocx
-wstdecod.dll
-wstpager.ax
-wstrendr.ax
-xmlprov.dll
-xmlprovi.dll
-zipfldr.dll
-""".splitlines()
 
-for f in RegisterDlls:
-    wine('regsvr32.exe', f)
+################
+# RegisterDlls #
+################
+
+inf_data = """\
+[version]
+signature = $CHICAGO$
+
+[DefaultInstall]
+RegisterDlls = RegisterDllsSection
+
+[RegisterDllsSection]
+11,,quartz.dll      ,1
+11,,mfc40u.dll      ,1
+11,,mfc42.dll       ,1
+11,,mfc42u.dll      ,1
+
+
+11,,actxprxy.dll    ,1
+11,,advpack.dll     ,1
+11,,asctrls.ocx     ,1
+11,,atl.dll         ,1
+11,,bdaplgin.ax     ,1
+11,,browseui.dll    ,1
+11,,ddrawex.dll     ,1
+11,,devenum.dll     ,1
+11,,dinput.dll      ,1
+11,,dinput8.dll     ,1
+11,,dispex.dll      ,1
+11,,dmband.dll      ,1
+11,,dmcompos.dll    ,1
+11,,dmime.dll       ,1
+11,,dmloader.dll    ,1
+11,,dmscript.dll    ,1
+11,,dmstyle.dll     ,1
+11,,dmsynth.dll     ,1
+11,,dmusic.dll      ,1
+11,,dplayx.dll      ,1
+11,,dpnet.dll       ,1
+11,,dpnhpast.dll    ,1
+11,,dpnhupnp.dll    ,1
+11,,dpvacm.dll      ,1
+11,,dpvoice.dll     ,1
+11,,dpvvox.dll      ,1
+11,,dsdmo.dll       ,1
+11,,dsdmoprp.dll    ,1
+11,,dsprop.dll      ,1
+11,,dsquery.dll     ,1
+11,,dssenh.dll      ,1
+11,,dsuiext.dll     ,1
+11,,dswave.dll      ,1
+11,,dx7vb.dll       ,1
+11,,dx8vb.dll       ,1
+11,,dxdiagn.dll     ,1
+11,,dxmasf.dll      ,1
+11,,dxmrtp.dll      ,1
+11,,dxtmsft.dll     ,1
+11,,dxtrans.dll     ,1
+11,,encapi.dll      ,1
+11,,encdec.dll      ,1
+11,,flash.ocx       ,1
+11,,fontext.dll     ,1
+11,,hhctrl.ocx      ,1
+11,,ipsink.ax       ,1
+11,,jscript.dll     ,1
+11,,ksproxy.ax      ,1
+11,,kswdmcap.ax     ,1
+11,,l3codeca.acm    ,1
+11,,mp43dmod.dll    ,1
+11,,mp4sdmod.dll    ,1
+11,,mpg2data.ax     ,1
+11,,mpg2splt.ax     ,1
+11,,mpg4dmod.dll    ,1
+11,,mpg4ds32.ax     ,1
+11,,msadds32.ax     ,1
+11,,msdvbnp.ax      ,1
+11,,msdxm.ocx       ,1
+11,,msscds32.ax     ,1
+11,,msscript.ocx    ,1
+11,,msvbvm60.dll    ,1
+11,,mswebdvd.dll    ,1
+11,,mswmdm.dll      ,1
+11,,oledb32.dll     ,1
+11,,oledb32r.dll    ,1
+11,,oleprn.dll      ,1
+11,,proctexe.ocx    ,1
+11,,psisrndr.ax     ,1
+11,,qasf.dll        ,1
+11,,qcap.dll        ,1
+11,,qdv.dll         ,1
+11,,qdvd.dll        ,1
+11,,qedit.dll       ,1
+11,,query.dll       ,1
+11,,qutil.dll       ,1
+11,,scecli.dll      ,1
+11,,shdocvw.dll     ,1
+11,,shgina.dll      ,1
+11,,shimgvw.dll     ,1
+11,,shmedia.dll     ,1
+11,,shsvcs.dll      ,1
+11,,slbcsp.dll      ,1
+11,,slbiop.dll      ,1
+11,,stobject.dll    ,1
+11,,sysmon.ocx      ,1
+11,,tdc.ocx         ,1
+11,,vbicodec.ax     ,1
+11,,vbisurf.ax      ,1
+11,,vbscript.dll    ,1
+11,,vidcap.ax       ,1
+11,,wmadmod.dll     ,1
+11,,wmadmoe.dll     ,1
+11,,wmdmlog.dll     ,1
+11,,wmdmps.dll      ,1
+11,,wmm2ae.dll      ,1
+11,,wmm2ext.dll     ,1
+11,,wmm2filt.dll    ,1
+11,,wmm2fxa.dll     ,1
+11,,wmm2fxb.dll     ,1
+11,,wmp.ocx         ,1
+11,,wmpcd.dll       ,1
+11,,wmpcore.dll     ,1
+11,,wmphoto.dll     ,1
+11,,wmpui.dll       ,1
+11,,wmsdmod.dll     ,1
+11,,wmsdmoe.dll     ,1
+11,,wmsdmoe2.dll    ,1
+11,,wmspdmod.dll    ,1
+11,,wmspdmoe.dll    ,1
+11,,wmstream.dll    ,1
+11,,wmv8ds32.ax     ,1
+11,,wmvcore.dll     ,1
+11,,wmvdmod.dll     ,1
+11,,wmvdmoe2.dll    ,1
+11,,wmvds32.ax      ,1
+11,,wshcon.dll      ,1
+11,,wshext.dll      ,1
+11,,wshom.ocx       ,1
+11,,wstdecod.dll    ,1
+11,,wstpager.ax     ,1
+11,,wstrendr.ax     ,1
+11,,xmlprov.dll     ,1
+11,,xmlprovi.dll    ,1
+11,,zipfldr.dll     ,1
+"""
+
+inf_tmp = os.path.join(W_TEMP, os.path.basename(tempfile.NamedTemporaryFile(suffix=".inf").name))
+open(inf_tmp, "w").write(inf_data)
+wine("rundll32.exe", "setupapi.dll,InstallHinfSection", "DefaultInstall", "128", inf_tmp)
 
 
 
@@ -695,6 +722,7 @@ wine(prefix + '/share/wine/vcrun2005/vcredist_x86.exe', '/q')
 wine(prefix + '/share/wine/vcrun2008sp1/vcredist_x86.exe', '/q')
 wine(prefix + '/share/wine/vcrun2010sp1/vcredist_x86.exe', '/q')
 wine('wineboot.exe', '-r')
+
 
 
 ###########
@@ -710,10 +738,12 @@ wine("wineboot.exe", "-r")
 os.unsetenv("WINEDLLOVERRIDES")
 
 
+
 ####################
 # Direct3D setting #
 ####################
-plist   = subprocess.Popen(['/usr/sbin/system_profiler', 'SPDisplaysDataType'], stdout=subprocess.PIPE).communicate()[0]
+
+plist   = Popen(['/usr/sbin/system_profiler', 'SPDisplaysDataType'], stdout=PIPE).communicate()[0]
 vendor  = re.search('Vendor:.*(0x....)', plist).group(1)
 device  = re.search('Device ID: (0x....)', plist).group(1)
 reg = '''\
@@ -723,4 +753,7 @@ reg = '''\
 '''\
 .replace('__VideoPciDeviceID__', device)\
 .replace('__VideoPciVendorID__', vendor)
-subprocess.Popen([prefix + "/libexec/wine", "regedit.exe", "-"], stdin=subprocess.PIPE).communicate(reg)
+Popen([prefix + "/libexec/wine", "regedit.exe", "-"], stdin=PIPE).communicate(reg)
+
+
+shutil.rmtree(W_TEMP)
